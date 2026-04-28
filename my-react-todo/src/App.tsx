de52e490461d83062a26ed7257ec7188e2e5e3d6
joao aebi <joao.aebi@jobtrek.ch>
2026-04-28 13:04:44 +0200

    feat: Add some modern CSS base close #4

--- my-react-todo/src/App.tsx
+++ my-react-todo/src/App.tsx
@@ -10,8 +10,7 @@ export interface Task {
 }
 
 const App = () => {
-
-  const tasks = [
+  const tasks: Task[] = [
     { id: 1, text: "Tomate", date: "02/04/2026", description: "Rouge", completed: false },
     { id: 2, text: "Banana", date: "27/04/2026", description: "Jaune", completed: true },
     { id: 2, text: "Orange", date: "27/04/2026", description: "Orange", completed: true },
@@ -20,13 +19,23 @@ const App = () => {
   return (
     <main className="React-Todo">
       <h1>TodoList React</h1>
-      
-      <section className='CSSBase'>
-        <h4>Nouvelle Tâche</h4>
-        <input type="text" placeholder='Titre de la Tâche . . .' />
-        <button>+ Crée la Tâche</button>
+      <section className='CSSBase' id='ErrorMSG'>
+        <h3>Chamb requis manquant</h3>
+        <p>Le ( Nom ) de la tâche est obligatoire.</p>
       </section>
-      <TodoList tasks={tasks} />
+      <section className='CSSBase padding'>
+        <div>
+          <h4>Nouvelle Tâche</h4>
+          <p>Titre</p>
+          <input className='CSSBase' type="text" placeholder='Titre de la Tâche . . .' />
+        </div>
+        <button className='CSSBase' style={{justifyItems: 'right'}}>+ Crée la Tâche</button>
+      </section>
+
+      <section className='CSSBase padding' id='displayTasks'>
+        <h4>Liste des Tâches</h4>
+        <TodoList tasks={tasks} />
+      </section>
     </main>
   );
 };
--- my-react-todo/src/components/TodoItem.tsx
+++ my-react-todo/src/components/TodoItem.tsx
@@ -1,4 +1,6 @@
 import type { Task } from "../App";
+import Trash from "../medias/Trash.png";
+import Edit from "../medias/edit.png";
 
 interface TodoItemProps {
     todo: Task;
@@ -6,15 +8,24 @@ interface TodoItemProps {
 
 export default function TodoItem({ todo }: TodoItemProps) {
     return (
-        <div style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}>
-            <input type="checkbox" defaultChecked={todo.completed} />
-            <div>
-                <h3>{todo.text}</h3>
-                <p>Data: {todo.date}</p>
-                <p>{todo.description}</p>
+        <div className="Task">
+            <div id="taskInformations">
+                <input type="checkbox" defaultChecked={todo.completed} />
+                <div>
+                    <h3>{todo.text}</h3>
+                    <p>Date {todo.date}</p>
+                    <p>{todo.description}</p>
+                </div>
             </div>
-            <button>Edit</button>
-            <button>Effacer</button>
+
+            <div className="TaskButtons">
+                <button className="CSSBase Buttons" id="editButton">
+                    <img className="imgButton" src={Edit} alt="edit logo"/>
+                </button>
+                <button className="CSSBase Buttons" id="clearButton">
+                    <img className="imgButton" src={Trash} alt="Trash logo" />
+                </button>
+            </div>
         </div>
     );
 }
