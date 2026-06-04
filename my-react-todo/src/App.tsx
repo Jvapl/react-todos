ece284a148dac8f942089cd35f3fe0ba325498ed
joao aebi <joao.aebi@jobtrek.ch>
2026-06-04 08:50:35 +0200

    feat: add error handler message on top of the page close #11

--- my-react-todo/src/App.tsx
+++ my-react-todo/src/App.tsx
@@ -1,60 +1,75 @@
 import { useState, Suspense } from "react";
 import { fetchTodosAPI, type Task, createTodosAPI, deleteTodosAPI, type TaskRead, updateTodosAPI } from "./API/DataRecuperation";
-import  { AddTask } from "./components/NewTask";
+import { AddTask } from "./components/NewTask";
 import TodoList from "./components/TodoList";
 import { SortingPopover } from "./components/SortingPopover";
+import { ErrorBanner } from "./components/ErrorBanner";
 
 const App = () => {
-  
+  const [apiError, setApiError] = useState<string | null>(null)
   const [tasksPromise, setTasksPromise] = useState(() => fetchTodosAPI())
   const [filterType, setFilterType] = useState("none")
   const [sortType, setSortType] = useState("none")
+
   const handleNewTask = async (taskFromChild: Task) => {
-  console.log("New task reached from AddTask: ", taskFromChild)
-    
-  try {
-    await createTodosAPI(taskFromChild)
-    console.log('Task created')
-    setTasksPromise(fetchTodosAPI())
-    
-  }catch(error){
-    console.error('Could not create task: ', error);
-  }
-}
+    try {
+      setApiError(null)
+      await createTodosAPI(taskFromChild)
+      setTasksPromise(fetchTodosAPI())
+    } catch (error: unknown) {
+      if(error instanceof Error){} // instanceof système de verification d'heritage
+      if(error instanceof Error){
+        setApiError(error.message || "An error occurred while creating the task.")
+      }else{
+        setApiError("An unexpected error occurred.")
+      }
+    }
+  };
 
   const handleDeleteTask = async (id: number) => {
-      try {
+    try {
+      setApiError(null)
       await deleteTodosAPI(id)
       setTasksPromise(fetchTodosAPI())
-    }catch(error){
-      console.error('Error supression: ',error)
+    } catch (error: unknown) {
+      if(error instanceof Error){
+        setApiError(error.message || "An error occurred while deleting the task.")
+      }
+      setApiError("An unexpected error occurred.")
     }
-  }
+  };
   
   const handleEditTask = async (id: number, updatedTask: Partial<TaskRead>) => {
     try {
+      setApiError(null)
       await updateTodosAPI(id, updatedTask)
       setTasksPromise(fetchTodosAPI())
-    }catch(error){
-      console.error('Error editing: ', error)
+    } catch (error: unknown) {
+      if(error instanceof Error){
+        setApiError(error.message || "An error occured while updating the task.")
+      }
+      setApiError("An unexpected error occurred.")
     }
   }
 
   return (
     <main className="React-Todo">
+      {apiError && (
+        <ErrorBanner 
+          message={apiError} 
+          onDismiss={() => setApiError(null)} 
+        />
+      )}
 
-      {/* Input Informations */}
       <h1>TodoList React</h1>
 
       <section className='CSSBase padding' id='NewTaskHolder'>
         <div id="informations">
           <h2>New Task</h2>
           <AddTask onAddTask={handleNewTask}></AddTask>
-          {/* tout ce qui est input */}
         </div>
       </section>
 
-      {/* Tasks Display */}
       <section className='CSSBase padding'>
         <div className="sortDiv">
           <h4>Tasks List</h4>
